9866654851aa335349d90bf99cf53832dd71ff80
Mahfawk <monkeygidr0@gmail.com>
2026-03-15 21:00:41 +0530

    Exctraction et ajout de function disabled

---
+++ script.js
@@ -0,0 +1,30 @@
+let historique=[];
+let jour = historique.length==0 ? 0 : historique.length.at(-1).jour;
+let datyandroany;
+function maka_donnee(){
+    datyandroany= new Date().toLocaleDateString("fr-FR");
+    let verifcheckes= document.querySelectorAll('input[type="checkbox"]:checked');
+    let isachecked = verifcheckes.length;
+    let journee={"date":datyandroany,"point":isachecked,"jour":++jour};
+    historique.push(journee);
+    if(historique.length > 0 && datyandroany == historique.at(-1).date){
+            document.querySelector("#exporter").disabled=true;
+        }
+    console.log(historique);
+    console.log(historique.at(-1).date);
+    console.log(jour)
+}
+function ajout_activite(){
+    let list_act= document.querySelector(".activity_list");
+    let new_act=document.createElement("div");
+    new_act.setAttribute("id","activity")
+    let checke = document.createElement("input");
+    checke.setAttribute("type","checkbox");
+    let texte = document.createElement("input");
+    texte.setAttribute("type","text");
+    texte.setAttribute("placeholder","Entrer une activité");
+    texte.setAttribute("style","font-size:30px");
+    new_act.appendChild(checke); 
+    new_act.appendChild(texte);
+    list_act.appendChild(new_act);
+}
