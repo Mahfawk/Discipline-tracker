3de3484246d336cea7bd0c16b9caa9a32a32ccb5
Mahfawk <monkeygidr0@gmail.com>
2026-03-15 21:04:39 +0530

    Ajout de disabled

--- script.js
+++ script.js
@@ -2,9 +2,10 @@ let historique=[];
 let jour = historique.length==0 ? 0 : historique.length.at(-1).jour;
 let datyandroany;
 function maka_donnee(){
-    datyandroany= new Date().toLocaleDateString("fr-FR");
-    let verifcheckes= document.querySelectorAll('input[type="checkbox"]:checked');
-    let isachecked = verifcheckes.length;
+    datyandroany= new Date().toLocaleDateString("fr-FR"); //maka daty androany
+    let verifcheckes= document.querySelectorAll('input[type="checkbox"]:checked'); // checkbox valide
+    let isachecked = verifcheckes.length; // manisa checkbox valide
+
     let journee={"date":datyandroany,"point":isachecked,"jour":++jour};
     historique.push(journee);
     if(historique.length > 0 && datyandroany == historique.at(-1).date){
@@ -14,9 +15,9 @@ function maka_donnee(){
     console.log(historique.at(-1).date);
     console.log(jour)
 }
-function ajout_activite(){
-    let list_act= document.querySelector(".activity_list");
-    let new_act=document.createElement("div");
+function ajout_activite(){ 
+    let list_act= document.querySelector(".activity_list"); 
+    let new_act=document.createElement("div"); //definition new element
     new_act.setAttribute("id","activity")
     let checke = document.createElement("input");
     checke.setAttribute("type","checkbox");
@@ -24,7 +25,7 @@ function ajout_activite(){
     texte.setAttribute("type","text");
     texte.setAttribute("placeholder","Entrer une activité");
     texte.setAttribute("style","font-size:30px");
-    new_act.appendChild(checke); 
-    new_act.appendChild(texte);
-    list_act.appendChild(new_act);
+    new_act.appendChild(checke); //mise en place de checke
+    new_act.appendChild(texte); //mise en place de texte
+    list_act.appendChild(new_act); // combinaison
 }
